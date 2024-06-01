de61bec31b849369ab4d3baf7d312c0e8b2d0d4c
camizgon <camizgon22@hotmail.com>
2024-06-01 14:12:13 -0400

    Notas Carlos

---
+++ M5 - Javascript Avanzado/RepasoCarlos/Notas.js
@@ -0,0 +1,24 @@
+/* LÓGICA DE PROGRAMACIÓN */
+//Se piensa de manera ambigua intentan imprementar en un cmputador soluciones o ideas que simulan un lenguaje humano para esto deben cumplir ser un lenguaje de conmtexto, el lenguaje humano es contexto al no entenderlo se complica la converzación, 
+
+// En los lenguajes no pasa esto ya que cumplan categorias de CATEGORIAS DE TOKEN (palabras dlel códugo fuente) source code
+//uft-8 => 
+// código escrito : es el que se ejecuta según lo que yo le digo al programana.
+
+//código fuente: es el código que se almacena ejecutando en la memoria ram con propiedades para consumir en js es 
+
+//código vivo: Es el código que se crea en memoria con propiedades para consumir en js es y es una representacipon en la memoria
+
+
+/*---------CATEGORIAS DE TOKEN------------*/
+
+//LINKK REPASO CONTENIDO
+//https://codepen.io/carlospizarro/pen/abrJVBj
+//https://codepen.io/carlospizarro/pen/abrJENe
+
+
+
+//typeof: validador de datos que nos ayuda a verificar el tipo de datos primitivoque tendre que aplicar es un operador
+
+/*VARIABLES */
+//son nomnres que tendran un espacio en la memoria 
